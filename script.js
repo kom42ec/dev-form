4d98e1a090486dcea1911c7e449e792860a7d5fe
kom42ec <69169748+nikolakomazec@users.noreply.github.com>
2022-11-05 21:47:03 +0100

    modal added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const overlayEl = document.querySelector(".overlay");
+const logIn = document.getElementById("login");
+const modalEl = document.getElementById("login-box");
+
+logIn.onclick = function () {
+  overlayEl.style.display = "block";
+  modalEl.style.display = "block";
+};
+
+overlayEl.onclick = function () {
+  overlayEl.style.display = "none";
+  modalEl.style.display = "none";
+};
